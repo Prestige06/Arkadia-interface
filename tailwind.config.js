872431168ff5cdecebafe01d503c1b422379e80e
Prestige06 <115601062+Prestige06@users.noreply.github.com>
2025-04-09 04:09:47 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 // tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"
+           "./app/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}", ],
   theme: {
     extend: {},
   },
