bbaae45b5c24dc32e3ef7c09a899e2b286ffbb83
Prestige06 <115601062+Prestige06@users.noreply.github.com>
2025-04-09 03:14:52 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      borderRadius: {
-        '2xl': '1rem'
-      },
-      colors: {
-        arkadia: '#98f5e1'
-      }
-    }
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
